58129ff893b47e71e604bce29a4187976279d9cc
Pitura <pa.pitura@gmail.com>
2024-05-13 08:43:17 +0200

    API vulnerabiliti resolved

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCvDJOBkTm-OFTE1yWaQQeYiaW9PVLsTCM',
-    authDomain: 'geriatria30plus.firebaseapp.com',
-    projectId: 'geriatria30plus',
-    storageBucket: 'geriatria30plus.appspot.com',
-    messagingSenderId: '49835649931',
-    appId: '1:49835649931:web:aa9fb85e22766375461d8e',
-    measurementId: 'G-GJTJVGWQXH',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
